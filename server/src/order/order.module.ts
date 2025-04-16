b18b5ff57acb26dd4fc47675bef07396bcb8e61f
cowboiz <dovanhoang1201@gmail.com>
2025-04-16 11:47:03 +0700

    fix order

--- server/src/order/order.module.ts
+++ server/src/order/order.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
-import { OrderGateway } from './order.gateway.js';
+import { OrderGateway } from './order.gateway';
 import { Order } from './entities/order.entity';
 import { OrderItem } from './entities/order-item.entity';
 import { Table } from '../table/entities/table.entity';
