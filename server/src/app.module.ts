bf89606935d7b09c5cddaea466fcb1547b1b1752
cowboiz <dovanhoang1201@gmail.com>
2025-04-27 13:00:44 +0700

    Backend folder structure

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,6 +7,17 @@ import { UsersModule } from './users/users.module';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { IngredientsModule } from './ingredients/ingredients.module';
 import { IngredientBatchesModule } from './ingredient-batches/ingredient-batches.module';
+import { AuthModule } from './auth/auth.module';
+import { TablesModule } from './tables/tables.module';
+import { DishesModule } from './dishes/dishes.module';
+import { MenusModule } from './menus/menus.module';
+import { OrdersModule } from './orders/orders.module';
+import { KitchenModule } from './kitchen/kitchen.module';
+import { BatchesModule } from './batches/batches.module';
+import { SuppliersModule } from './suppliers/suppliers.module';
+import { InventoryModule } from './inventory/inventory.module';
+import { FinancialModule } from './financial/financial.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
   imports: [
@@ -15,6 +26,17 @@ import { IngredientBatchesModule } from './ingredient-batches/ingredient-batches
     RestaurantsModule,
     IngredientsModule,
     IngredientBatchesModule,
+    AuthModule,
+    TablesModule,
+    DishesModule,
+    MenusModule,
+    OrdersModule,
+    KitchenModule,
+    BatchesModule,
+    SuppliersModule,
+    InventoryModule,
+    FinancialModule,
+    ReportsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
